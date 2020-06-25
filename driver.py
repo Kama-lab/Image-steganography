51613e109bb3934b69fc2bdd52367c6089330572
solie <soliev.kamronbek@gmail.com>
2020-06-25 21:26:41 +0300

    main driver function

--- driver.py
+++ driver.py
@@ -1,12 +1,13 @@
 import cv2
+import binascii
 
 TARGET_BITS = 2
-CHANNELS = 3
 
 def open_image(path):
 	image = cv2.imread(path)
-	cv2.imshow("image",image)
-	cv2.waitKey(0)
+	#cv2.imshow("image",image)
+	#cv2.waitKey(0)
+	return image
 
 
 def convert_to_bin(pixelValues):
@@ -30,13 +31,55 @@ def insert_bits(pixel,bitsToHide):
 	n=0
 	for value in binPixels:
 		if(len(value)>=TARGET_BITS):
-			print(n,value)
 			newBinValues.append(value[:-TARGET_BITS] + str(bitsToHide[n]))
-			print(n,value)
 		else:
 			newBinValues.append(bitsToHide)
 		n+=1
 	return convert_to_dec(newBinValues)
+
+def calc_image_storage(imageSize,channels):
+	width, height = imageSize
+	storage = width*height*channels*TARGET_BITS
+	#print(f"Available:{storage}bits or {storage/8}bytes")
+	return storage
+
+
+
+def hide_text(image,text):
+	image = cv2.imread(image)
+	file = open(text,"r")
+	print(image)
+	nBits = []
+	nthBit = 0
+	nthChar = 0
+	char = file.read(nthChar+1)[nthChar:]
+	#binChar = bin(int(binascii.hexlify(char), 16))[2:]
+	binChar = bin(int.from_bytes(char.encode(),"big"))
+	print(binChar)
+	for row in image:
+		for col in row:
+			for i in range(3):
+				try:
+					nBits.append(binChar[nthBit]+binChar[nthBit+1])
+					nthBit+=2
+				except IndexError:
+					nthChar+=1
+					nthBit = 0
+					char = file.read(nthChar+1)[nthChar:]
+					nBits.append(binChar[nthBit]+binChar[nthBit+1])
+			print(nBits)
+			#print(map("".join(),[nBits[:2],nBits[2:4],nBits[4:]]))
+			image[row,col] = insert_bits(col,[nBits[:2],nBits[2:4],nBits[4:5]])
+			nBits = []
+
+	print(image)
+	cv2.imshow("img",image)
+	cv2.waitKey(0)
+			
+
+#hide_text(,"res/test.txt")
+hide_text("res/test_image2.png","res/test.txt")
+
 
 
 
