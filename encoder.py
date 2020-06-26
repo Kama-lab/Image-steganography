920653eaa0c796e119a32f820ef6f04a0d1e1fbd
solie <soliev.kamronbek@gmail.com>
2020-06-26 15:18:42 +0300

    implementation of one and two bits insertion

--- encoder.py
+++ encoder.py
@@ -2,13 +2,7 @@ import cv2
 import binascii
 import numpy as np
 
-TARGET_BITS = 2
-
-def open_image(path):
-	image = cv2.imread(path)
-	#cv2.imshow("image",image)
-	#cv2.waitKey(0)
-	return image
+TARGET_BITS = 1
 
 
 def convert_to_bin(pixelValues):
@@ -44,22 +38,35 @@ def calc_image_storage(imageSize,channels):
 	#print(f"Available:{storage}bits or {storage/8}bytes")
 	return storage
 
+def one_bit_slicing(bits):
+	return [bits[0],bits[1],bits[2]]
 
+def two_bit_slicing(bits):
+	return [bits[0]+bits[1],bits[2]+bits[3],bits[4]+bits[5]]
 
 def encode(image,text):
+	steps_per_pixel = TARGET_BITS*3
+	function = None
+	if TARGET_BITS==1:
+		function = one_bit_slicing
+	elif TARGET_BITS==2:
+		function = two_bit_slicing
+	else:
+		pass
 	image = cv2.imread(image)
 	storage_size = calc_image_storage(image.shape[:2],image.shape[2])
 	written_size = 0
 	file = open(text,"r")
-	print(image)
 	nBits = []
 	nthBit = 0
 	nthChar = 0
+
 	char = file.read(nthChar+1)[nthChar:]
 	binChar = bin(int.from_bytes(char.encode(),"big"))[2:]
+
 	for row in range(image.shape[0]):
 		for col in range(image.shape[1]):
-			for i in range(6):
+			for i in range(steps_per_pixel):
 				try:
 					nBits.append(binChar[nthBit])
 					nthBit+=1
@@ -67,18 +74,20 @@ def encode(image,text):
 					nthChar+=1
 					nthBit = 0
 					char = file.read(nthChar+1)[nthChar:]
-					print("Char:",char)
 					binChar = bin(int.from_bytes(char.encode(),"big"))[2:]
 					nBits.append(binChar[nthBit])
-			image[row][col] = insert_bits(image[row][col],[nBits[0]+nBits[1],nBits[2]+nBits[3],nBits[4]+nBits[5]])
-			written_size += 6
+			try:
+				image[row][col] = insert_bits(image[row][col],function(nBits))
+			except TypeError:
+				print("Invalid target bits")
+				return 0
+
+			written_size += steps_per_pixel
 			print(f"{storage_size}/{written_size}")
 			nBits = []
-
-	print(image)
+	cv2.imwrite("one_bit_steg.png",image)
 	cv2.imshow("img",image)
 	cv2.waitKey(0)
 			
 
 encode("res/test_image2.png","res/test.txt")
-
